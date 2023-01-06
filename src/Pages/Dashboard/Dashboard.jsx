9cc0e7bfed2877d266b07f4a4095125487d93d41
shahzaibansari123 <ansarishahzaib567@gmail.com>
2023-01-06 17:23:44 +0500

    workig on it

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -8,196 +8,145 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios'
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import axios from "axios";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const [food,setFood]=useState();
+  const [food, setFood] = useState();
 
-  
-    const [item, setItem] = React.useState("burger");
-  
-    const handleChange = (event) => {
-      setItem(event.target.value);
-    };
+  const [item, setItem] = React.useState("burger");
 
-useEffect(() => {
+  const handleChange = (event) => {
+    setItem(event.target.value);
+  };
+
+  useEffect(() => {
     axios
-    .get(`https://api.spoonacular.com/food/products/search?query=${item}&apiKey=189ae019736942bf877a6b4e0695ea8a`)
-    .then(function (response) {
-      console.log(response.data.products?.[0].title);
-      setFood(response?.data)
-      console.log(food)
-    })
-    .catch(function (error) {
-      console.log(error.message);
-
-    })
+      .get(
+        `https://api.spoonacular.com/food/products/search?query=${item}&apiKey=189ae019736942bf877a6b4e0695ea8a`
+      )
+      .then(function (response) {
+        console.log(response.data.products?.[0].title);
+        alert(response.data.products?.[0].title);
+        setFood(response?.data);
+        // console.log(food);
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
     // .then(function () {});
-}, [item])
+  }, [item]);
 
-  
   return (
     <>
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
-            CODEJAM FOOD APP
-          </Typography>
-          <Box>
-            <Button
-              onClick={() => navigate("/Favorites")}
-              sx={{ color: "white", textDecoration: "none" }}
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar component="nav">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{ mr: 2, display: { sm: "none" } }}
             >
-              Favorites
-            </Button>
-            <Button
-              onClick={() => navigate("/")}
-              sx={{
-                backgroundColor: "Red",
-                color: "white",
-                textDecoration: "none",
-              }}
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
             >
-              Logout
-            </Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
-    <div>hhh</div>
-    <Box sx={{ minWidth: 120,marginTop:"100px" }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Menu</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={item}
-          label="Menu"
-          onChange={handleChange}
-        >
-          <MenuItem value="burger">Burger</MenuItem>
-          <MenuItem value="pizza">Pizza</MenuItem>
-          <MenuItem value="biryani">Biryani</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-    {
-        !item ? 
-        <div>Select Food Item </div>:
+              CODEJAM FOOD APP
+            </Typography>
+            <Box>
+              <Button
+                onClick={() => navigate("/Favorites")}
+                sx={{ color: "white", textDecoration: "none" }}
+              >
+                Favorites
+              </Button>
+              <Button
+                onClick={() => navigate("/")}
+                sx={{
+                  backgroundColor: "Red",
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
+                Logout
+              </Button>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div>hhh</div>
+      <Box sx={{ minWidth: 120, marginTop: "100px" }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Menu</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={item}
+            label="Menu"
+            onChange={handleChange}
+          >
+            <MenuItem value="burger">Burger</MenuItem>
+            <MenuItem value="pizza">Pizza</MenuItem>
+            <MenuItem value="biryani">Biryani</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      {!item ? (
+        <div>Select Food Item </div>
+      ) : (
         <div>
-<Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+          {/* {food?.map((i) => (
+            <>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardHeader title={food?.products?.title} />
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image="/static/images/cards/paella.jpg"
+                  alt="Paella dish"
+                />
+                <CardContent>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                  ></Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                  </IconButton>
+                </CardActions>
+              </Card>
+            </>
+          ))} */}
+          
         </div>
-    }
+      )}
     </>
   );
-            }
+};
 
 export default Dashboard;
