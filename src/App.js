c4c7c6aebf2b14f5d0d15d29d9c3ff4d0a652096
shahzaibansari123 <ansarishahzaib567@gmail.com>
2023-01-06 13:03:48 +0500

    workig on it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import {db} from './firebase'
+import Signup from "./Pages/Signup/Signup";
+import Login from "./Pages/Login/Login";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Favorites from "./Pages/Favorites/Favorites";
 
 function App() {
+  // console.log(db);
   return (
     <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={}  />
-    </Routes>
+      <Routes>
+        <Route exact path="/" element={<Signup />} />
+        <Route exact path="/Login" element={<Login />} />
+        <Route exact path="/Favorites" element={<Favorites />} />
+        <Route exact path="/Dashboard" element={<Dashboard />} />
+      </Routes>
     </BrowserRouter>
-
-   
-   
   );
 }
 
---
+++ src/Pages/Signup/Signup.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import  Grid from "@mui/material/Grid";
+// import Item from '@mui/material/Item'
+import { styled } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import SignupForm from "../../Components/SignupForm";
+
+const Signup = () => {
+    const Item = styled(Paper)(({ theme }) => ({
+        // backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        // ...theme.typography.body2,
+        // padding: theme.spacing(1),
+        // textAlign: 'center',
+        // color: theme.palette.text.secondary,
+      }));
+  return (
+    <div>
+      {/* <h1>signup</h1> */}
+      <Grid container spacing={2} >
+        <Grid item  xs={0} sm={0}  md={6} lg={8} xl={8} >
+          <Item><img src="https://source.unsplash.com/random/900x700/?food" width="100%" height="100%" /></Item>
+        </Grid>
+        <Grid item xs={0} sm={0} md={6} lg={4} xl={4}>
+         <SignupForm />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Signup;
