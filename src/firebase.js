0b4c1f3da17b07bcebab45c98425a56bf49eafc5
fsidd107 <fsiddiqui107@gmail.com>
2023-01-06 12:02:19 +0500

    feat : firebase integrated

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVOvisbIRqKHiKcAigkXvINgO-kTqDoWg",
+  authDomain: "codejam23foodapp.firebaseapp.com",
+  projectId: "codejam23foodapp",
+  storageBucket: "codejam23foodapp.appspot.com",
+  messagingSenderId: "84735532636",
+  appId: "1:84735532636:web:10af34ab7bd4101c8d1099"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth();
+export const storage = getStorage(app);
